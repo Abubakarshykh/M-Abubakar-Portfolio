f044c3e02e9bf17e3ef0592116f8022339bcc833
Abubakarshykh <sheikhabubaker498@gmail.com>
2026-05-11 21:25:22 +0500

    chore: switch from gsap-trial to standard gsap (now 100% free)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { SplitText } from "gsap/SplitText";
 import HoverLinks from "./HoverLinks";
 import { gsap } from "gsap";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import "./styles/Navbar.css";
 
-gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger, SplitText);
 export let smoother: ScrollSmoother;
 
 const Navbar = () => {
